14c98705f11267aa62cd793eaac3fc1b848664fe
harrymcvey <118142090+harrymcvey@users.noreply.github.com>
2023-12-10 16:05:02 -0600

    first commit

---
+++ homepage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Homepage</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to the Administration Panel</h1>
+    </header>
+
+    <main>
+        <nav>
+            <ul>
+                <li><a href='eventsForm.php'>Add New Event</a></li>
+                <li><a href='updateEvent.php'>Show List of Events to update</a></li>
+                <li><a href='deleteEvents.php'>Show List of Events to delete</a></li>
+                <li><a href='logout.php'>Logout of Administrator</a></li>
+            </ul>
+        </nav>
+    </main>
+</body>
+</html>
